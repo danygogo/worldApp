16779393d0b0afe12d29b371b4de87510b0619bc
Daniela Gonzalez <danygogo@gmail.com>
2021-09-12 21:07:44 -0600

    the input.component was created

---
+++ src/app/country/components/input/input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+
+  term: string = " ";
+
+  search(){
+    
+  }
+
+}
