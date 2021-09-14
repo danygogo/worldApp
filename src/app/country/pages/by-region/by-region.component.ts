b1ff0c207e2c6680e3c6b9fdb4132b3b78ecbc18
Daniela Gonzalez <danygogo@gmail.com>
2021-09-14 17:49:03 -0600

    search by region was implemented

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RegionResponse } from '../../interfaces/region.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styleUrls: ['./by-region.component.css']
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
 
-  constructor() { }
+  regions: string[] = ["africa", "americas", "asia", "europe", "oceania"];
+  activeRegion: string = " ";
+  countries: RegionResponse[] = [];
+  errorFound: boolean = false;
+  elements: number = 0;
 
-  ngOnInit(): void {
+  constructor(private countryService: CountryService) { }
+
+  activateRegion(region: string){
+    this.activeRegion = region;
+    //TODO: LLAMAR AL SERVICIO
+    this.countryService.searchRegion(this.activeRegion)
+    .subscribe((resp) => {
+      this.countries = resp;
+    }, (err) =>{
+      this.errorFound = true;
+      this.countries = [];
+    })
   }
-
 }
